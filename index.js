8daf204eb2718d85a0f23434fdfd4d6dcd2b3677
Aseke <askar.toleuovv@gmail.com>
2025-02-16 19:53:07 +0500

    fix backedn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.put("/api/orders/:id", async (req, res) => {
                 new: true,
                 runValidators: true
             })
-        res.status(201)
+        res.status(201).json({success: true})
     }
     catch(error) {
         res.status(400).json(error);
